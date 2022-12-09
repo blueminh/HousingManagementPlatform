a91478a7c455e5e2b60b1ff61d1ade4ba6a9fbd8
Nguyen Hoang Minh <nguyenhoangminh@student.tudelft.nl>
2022-12-10 00:23:00 +0100

    quick fix for manageUserService

--- Code/hoa-microservice/src/main/java/sem/hoa/Application.java
+++ Code/hoa-microservice/src/main/java/sem/hoa/Application.java
@@ -2,11 +2,13 @@ package sem.hoa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Example microservice application.
  */
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "sem.hoa.domain.services")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
