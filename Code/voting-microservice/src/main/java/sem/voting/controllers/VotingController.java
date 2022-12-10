9d43877365cda7c76ac40278766d5c3784c8fd02
Giacomo Pezzali <g.pezzali@student.tudelft.nl>
2022-12-11 00:45:50 +0100

    Skeleton of endopoints

--- Code/voting-microservice/src/main/java/sem/voting/controllers/VotingController.java
+++ Code/voting-microservice/src/main/java/sem/voting/controllers/VotingController.java
@@ -3,8 +3,12 @@ package sem.voting.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import sem.voting.authentication.AuthManager;
+import sem.voting.domain.proposal.ProposalRepository;
+import sem.voting.models.ProposalCreationRequestModel;
+import sem.voting.models.ProposalCreationResponseModel;
 
 /**
  * Hello World example controller.
@@ -20,7 +24,7 @@ public class VotingController {
     /**
      * Instantiates a new controller.
      *
-     * @param authManager Spring Security component used to authenticate and authorize the user
+     * @param authManager        Spring Security component used to authenticate and authorize the user
      */
     @Autowired
     public VotingController(AuthManager authManager) {
@@ -28,23 +32,65 @@ public class VotingController {
     }
 
     /**
-     * Adds a proposal to vote on.
+     * Example endpoint.
      *
-     * @return 200 if the proposal has been added successfully.
+     * @return 200 and say hi to the currently logged-in user.
      */
-    @PostMapping("/propose")
-    public ResponseEntity<String> addProposal() {
+    @PostMapping("/example")
+    public ResponseEntity<String> helloWorld() {
         return ResponseEntity.ok("Hello " + authManager.getNetId());
     }
 
-    // ToDo: add APIs
-    /*
-    - send results (i.e. all expired proposals of an HOA)
-    - cast a vote
-    - remove a vote
-    - add a new proposal
-    - start voting on a new proposal
-    - get all proposals of an HOA
+    /**
+     * Endpoint to create a new proposal.
+     *
+     * @param request model of the request
+     * @return 200 if creation is successful
+     *      400 if request is not complete
      */
+    @PostMapping("/propose")
+    public ResponseEntity<ProposalCreationResponseModel> addProposal(
+            @RequestBody ProposalCreationRequestModel request) {
+        if (request == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        // ToDo
+        return ResponseEntity.ok(null);
+    }
 
+    /**
+     * Endpoint to start voting on a proposal.
+     *
+     * @return 200 if transition is possible
+     *      400 otherwise
+     */
+    @PostMapping("/start")
+    public ResponseEntity<String> beginVoting() {
+        // ToDo
+        return ResponseEntity.ok("");
+    }
+
+    @PostMapping("/vote")
+    public ResponseEntity<String> castVote() {
+        // ToDo
+        return ResponseEntity.ok("");
+    }
+
+    @PostMapping("/vote/remove")
+    public ResponseEntity<String> removeVote() {
+        // ToDo
+        return ResponseEntity.ok("");
+    }
+
+    @PostMapping("/results")
+    public ResponseEntity<String> getResults() {
+        // ToDo
+        return ResponseEntity.ok("");
+    }
+
+    @PostMapping("/info")
+    public ResponseEntity<String> listProposals() {
+        // ToDo
+        return ResponseEntity.ok("");
+    }
 }
