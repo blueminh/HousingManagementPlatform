b967a2c0155087a6492542b5c206ebe647839f0e
Atanas Kichukov <A.B.Kichukov@student.tudelft.nl>
2022-12-19 17:49:39 +0100

    pipeline fix

--- Code/hoa-microservice/src/main/java/sem/hoa/domain/services/HOAService.java
+++ Code/hoa-microservice/src/main/java/sem/hoa/domain/services/HOAService.java
@@ -12,45 +12,50 @@ public class HOAService {
     private final transient HOARepository hoaRepository;
 
     public HOAService(HOARepository hoaRepository) {
-    this.hoaRepository = hoaRepository;
-  }
-
-    public void createNewHOA(HOA hoa) {
-    // TODO do some checks here
-      try {
-        //System.out.println(hoa.toString());
-        hoaRepository.save(hoa);
-        System.out.println("new HOA created:" + hoa.getHoaName());
-      } catch(Exception e) {
-        System.out.println("unable to save new HOA");
-      }
+        this.hoaRepository = hoaRepository;
     }
 
-  /**
-   * Either find the name of the HOA or the hoaID.
-   *
-   * @param hoaName make this null or empty if hoaID is used
-   * @param hoaID   id of the HOA
-   * @return the start time and end time of the HOA's board election
-   */
+    /**
+    * Adds the given HOA to the HOA repository.
+    *
+    * @param hoa = hoa to be added to the repository
+    */
+    public void createNewHOA(HOA hoa) {
+        // TODO do some checks here
+        try {
+            //System.out.println(hoa.toString());
+            hoaRepository.save(hoa);
+            System.out.println("new HOA created:" + hoa.getHoaName());
+        } catch (Exception e) {
+            System.out.println("unable to save new HOA");
+        }
+    }
+
+    /**
+    * Either find the name of the HOA or the hoaID.
+    *
+    * @param hoaName make this null or empty if hoaID is used
+    * @param hoaID   id of the HOA
+    * @return the start time and end time of the HOA's board election
+    */
     public Pair<Long, Long> findBoardElectionStartTime(@Nullable String hoaName, int hoaID) {
-      Optional<HOA> hoa;
-      if (hoaName != null) {
-        hoa = hoaRepository.findByHoaName(hoaName);
-      }  else {
-        hoa = hoaRepository.findById(hoaID);
-      }
-      if (hoa.isEmpty()) {
-        return null;
-      }
-      return new Pair<Long, Long>(hoa.get().getElectionStartTime(), hoa.get().getElectionEndTime());
+        Optional<HOA> hoa;
+        if (hoaName != null) {
+            hoa = hoaRepository.findByHoaName(hoaName);
+        }  else {
+            hoa = hoaRepository.findById(hoaID);
+        }
+        if (hoa.isEmpty()) {
+            return null;
+        }
+        return new Pair<Long, Long>(hoa.get().getElectionStartTime(), hoa.get().getElectionEndTime());
     }
 
     public Optional<HOA> findHOAByName(String hoaName) {
-      return hoaRepository.findByHoaName(hoaName);
+        return hoaRepository.findByHoaName(hoaName);
     }
 
     public Optional<HOA> findHOAByID(int hoaID) {
-      return hoaRepository.findById(hoaID);
+        return hoaRepository.findById(hoaID);
     }
 }
