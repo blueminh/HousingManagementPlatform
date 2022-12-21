72c48f27146e23a59300fb8329439ed28ab61ab6
Konstantin <K.S.Mirinski@student.tudelft.nl>
2022-12-21 16:07:36 +0100

    needed fields for testing

--- Code/hoa-microservice/src/test/java/sem/hoa/controllers/RuleControllerTest.java
+++ Code/hoa-microservice/src/test/java/sem/hoa/controllers/RuleControllerTest.java
@@ -1,24 +1,39 @@
 package sem.hoa.controllers;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import sem.hoa.authentication.AuthManager;
+import sem.hoa.authentication.JwtTokenVerifier;
+import sem.hoa.domain.services.HOARepository;
+import sem.hoa.domain.services.HOAService;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-class RuleControllerTest {
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles({"test", "mockTokenVerifier", "mockAuthenticationManager"})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@AutoConfigureMockMvc
+public class RuleControllerTest {
 
-    @Test
-    void displayRules() {
-    }
+    @Autowired
+    private MockMvc mockMvc;
 
-    @Test
-    void addRule() {
-    }
+    @Autowired
+    private transient JwtTokenVerifier mockJwtTokenVerifier;
 
-    @Test
-    void editRule() {
-    }
+    @Autowired
+    private transient AuthManager mockAuthenticationManager;
 
-    @Test
-    void deleteRule() {
-    }
+    @Autowired
+    private transient HOAService hoaServiceMock;
+
+    @Autowired
+    private transient HOARepository hoaRepoMock;
 }
