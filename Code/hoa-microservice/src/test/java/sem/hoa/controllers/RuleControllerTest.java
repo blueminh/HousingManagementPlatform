304b3d0a5247d972320680f3266fe41ee9170df7
Konstantin <K.S.Mirinski@student.tudelft.nl>
2022-12-21 15:57:34 +0100

    added a class for testing

---
+++ Code/hoa-microservice/src/test/java/sem/hoa/controllers/RuleControllerTest.java
@@ -0,0 +1,24 @@
+package sem.hoa.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RuleControllerTest {
+
+    @Test
+    void displayRules() {
+    }
+
+    @Test
+    void addRule() {
+    }
+
+    @Test
+    void editRule() {
+    }
+
+    @Test
+    void deleteRule() {
+    }
+}
