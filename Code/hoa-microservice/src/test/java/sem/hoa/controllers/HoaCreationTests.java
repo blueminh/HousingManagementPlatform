d618a39a55b425bfb365a4048a395d2961488916
Atanas Kichukov <A.B.Kichukov@student.tudelft.nl>
2022-12-22 16:52:01 +0100

    testing bad requests

--- Code/hoa-microservice/src/test/java/sem/hoa/controllers/HoaCreationTests.java
+++ Code/hoa-microservice/src/test/java/sem/hoa/controllers/HoaCreationTests.java
@@ -22,7 +22,6 @@ import sem.hoa.utils.JsonUtil;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -50,6 +49,11 @@ public class HoaCreationTests {
     private transient HOARepository hoaRepoMock;
 
 
+    /**
+     * try to create a HOA normally using the endpoint for it.
+     *
+     * @throws Exception
+     */
     @Test
     public void createOne() throws Exception {
         // Arrange
@@ -76,13 +80,81 @@ public class HoaCreationTests {
 
         resultActions.andExpect(status().isOk());
 
-        HOA given = new HOA(request.hoaName, request.userCountry, request.userCity);
         HOA responded = JsonUtil
                 .deserialize(resultActions.andReturn().getResponse().getContentAsString(),
-                HOA.class);
+                        HOA.class);
+
         HOA saved = hoaRepoMock.findById(responded.getId()).orElseThrow();
 
+        HOA given = new HOA(request.hoaName, request.userCountry, request.userCity);
+
         assertThat(saved.getHoaName()).isEqualTo(given.getHoaName());
+        assertThat(saved.getCity()).isEqualTo(given.getCity());
+        assertThat(saved.getCountry()).isEqualTo(given.getCountry());
+        assertThat(saved.getId()).isEqualTo(1);
+    }
+
+    /**
+     *  Try to pass a blank String or null or an int<0 as one of the variables
+     *  should return bad request and nothing should be saved.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void createBadRequest() throws Exception {
+        // Arrange
+        // Notice how some custom parts of authorisation need to be mocked.
+        // Otherwise, the integration test would never be able to authorise as the authorisation server is offline.
+        when(mockAuthenticationManager.getNetId()).thenReturn("ExampleUser");
+        when(mockJwtTokenVerifier.validateToken(anyString())).thenReturn(true);
+        when(mockJwtTokenVerifier.getNetIdFromToken(anyString())).thenReturn("ExampleUser");
+
+        HoaModifyDTO request = new HoaModifyDTO();
+        request.setHoaName("exampleName");
+
+        request.setUserCity("");
+
+        request.setUserCountry("exUserCountry");
+
+        request.setUserStreet("Jump Street");
+        request.setUserHouseNumber(21);
+        request.setUserPostalCode("JUMP");
+
+
+        ResultActions resultActions = mockMvc.perform(post("/createHOA")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer MockedToken")
+                .content(JsonUtil.serialize(request)));
+
+        resultActions.andExpect(status().isBadRequest());
+
+        request.setUserCity(null);
+
+        resultActions = mockMvc.perform(post("/createHOA")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer MockedToken")
+                .content(JsonUtil.serialize(request)));
+
+        resultActions.andExpect(status().isBadRequest());
+
+        request.setUserCity("null");
+
+        resultActions = mockMvc.perform(post("/createHOA")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer MockedToken")
+                .content(JsonUtil.serialize(request)));
+
+        resultActions.andExpect(status().isOk());
+
+        request.setUserHouseNumber(-2);
+
+        resultActions = mockMvc.perform(post("/createHOA")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer MockedToken")
+                .content(JsonUtil.serialize(request)));
+
+        resultActions.andExpect(status().isBadRequest());
 
     }
+
 }
