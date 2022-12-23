05cd4f0ce1d67317f46810332637403e582fb849
Giacomo Pezzali <g.pezzali@student.tudelft.nl>
2022-12-23 08:58:45 +0100

    typo

--- Code/voting-microservice/src/main/java/sem/voting/communication/HoaCommunication.java
+++ Code/voting-microservice/src/main/java/sem/voting/communication/HoaCommunication.java
@@ -155,7 +155,7 @@ public class HoaCommunication {
      *
      * @param username  username of the user
      * @param hoaId     hoaId
-     * @return whether the user is a ember of this hoa
+     * @return whether the user is a member of this hoa
      * @throws Exception either a bad request or response has error
      */
     public static boolean checkUserIsMemberOfThisHoa(String username, int hoaId) throws Exception {
