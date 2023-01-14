13ed2d79c67ad0378251e8c9e01d3243fbf3d7e4
Máté <oszkomate@student.tudelft.nl>
2023-01-14 18:53:14 +0100

    Refactored HOAModifyDTO's public attributes to private

--- Code/hoa-microservice/src/main/java/sem/hoa/controllers/HoaController.java
+++ Code/hoa-microservice/src/main/java/sem/hoa/controllers/HoaController.java
@@ -74,14 +74,14 @@ public class HoaController {
             hoaService.checkHoaModifyDTO(request);
 
             //Creation
-            Hoa newHOA = new Hoa(request.hoaName, request.userCountry, request.userCity);
+            Hoa newHOA = new Hoa(request.getHoaName(), request.getUserCountry(), request.getUserCity());
 
             hoaService.createNewHOA(newHOA);
 
             memberManagementService
                 .addMembership(new Membership(authManager.getUsername(), newHOA.getId(), true,
-                    request.userCountry, request.userCity,
-                    request.userStreet, request.userHouseNumber, request.userPostalCode,
+                    request.getUserCountry(), request.getUserCity(),
+                    request.getUserStreet(), request.getUserHouseNumber(), request.getUserPostalCode(),
                     new Date().getTime(), new Date().getTime()));
 
             return ResponseEntity.ok(newHOA);
@@ -103,19 +103,19 @@ public class HoaController {
         try {
             //CHECKS
             hoaService.checkHoaModifyDTO(request);
-            if (!hoaService.hoaExistsByName(request.hoaName)) {
-                throw new HoaJoiningException("No such HOA with this name: " + request.hoaName);
+            if (!hoaService.hoaExistsByName(request.getHoaName())) {
+                throw new HoaJoiningException("No such HOA with this name: " + request.getHoaName());
             }
-            Hoa hoa = hoaService.findHoaByName(request.hoaName).get();
+            Hoa hoa = hoaService.findHoaByName(request.getHoaName()).get();
             if (memberManagementService
                 .findByUsernameAndHoaId(authManager.getUsername(), hoa.getId())
                 .isPresent()) {
                 throw new HoaJoiningException("User is already in this HOA"); //need explanation
             }
             Membership membership = new Membership(authManager.getUsername(),
-                hoaService.findHoaByName(request.hoaName).get().getId(), false,
-                request.userCountry, request.userCity, request.userStreet,
-                request.userHouseNumber, request.userPostalCode,
+                hoaService.findHoaByName(request.getHoaName()).get().getId(), false,
+                request.getUserCountry(), request.getUserCity(), request.getUserStreet(),
+                request.getUserHouseNumber(), request.getUserPostalCode(),
                 new Date().getTime(), -1L);
             if (!memberManagementService.addressCheck(hoa, membership)) {
                 throw new HoaJoiningException("Address not compatible with HOA area");
--- Code/hoa-microservice/src/main/java/sem/hoa/dtos/HoaModifyDTO.java
+++ Code/hoa-microservice/src/main/java/sem/hoa/dtos/HoaModifyDTO.java
@@ -4,10 +4,10 @@ import lombok.Data;
 
 @Data
 public class HoaModifyDTO {
-    public String hoaName;
-    public String userCountry;
-    public String userCity;
-    public String userStreet;
-    public int userHouseNumber;
-    public String userPostalCode;
+    private String hoaName;
+    private String userCountry;
+    private String userCity;
+    private String userStreet;
+    private int userHouseNumber;
+    private String userPostalCode;
 }
