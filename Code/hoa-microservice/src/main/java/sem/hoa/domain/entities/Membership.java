414801d6859d838203f80eb1151dc53f5dcb1d37
Máté <oszkomate@student.tudelft.nl>
2023-01-14 16:43:03 +0100

    Refactored membership class, improved weighted lines of code.

--- Code/hoa-microservice/src/main/java/sem/hoa/domain/entities/Membership.java
+++ Code/hoa-microservice/src/main/java/sem/hoa/domain/entities/Membership.java
@@ -1,8 +1,9 @@
 package sem.hoa.domain.entities;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -15,38 +16,47 @@ import javax.persistence.Table;
 @Table(name = "memberManagements")
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
 public class Membership {
     @Id
     @Column(name = "username", nullable = false)
     private String username;
 
+    @Getter
     @Id
     @Column(name = "hoaId", nullable = false)
     private int hoaId;
 
+    @Setter
+    @Getter
     // Enum type is complicated to be persisted in the database -> simply use a boolean value
     @Column(name = "isboardmember")
     private boolean isBoardMember;
 
+    @Getter
     @Column(name = "country")
     private String country;
 
+    @Getter
     @Column(name = "city")
     private String city;
 
     @Column(name = "street")
     private String street;
 
+    @Getter
     @Column(name = "houseNumber")
     private int houseNumber;
 
+    @Getter
     @Column(name = "postalCode")
     private String postalCode;
 
+    @Getter
     @Column(name = "joiningDate")
     private Long joiningDate;
 
+    @Getter
+    @Setter
     @Column(name = "joiningBoardDate")
     private Long joiningBoardDate;
 }
